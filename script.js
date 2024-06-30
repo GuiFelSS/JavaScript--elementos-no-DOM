1d4b3487bc7b4ed5b4f31d6c5cf64d949d828063
GuiFelSS <guilhermeflp6@gmail.com>
2024-06-30 17:29:22 -0300

    04. Desenvolvendo o temporizador
    esse commite é a finalização da parte 04. Desenvolvendo o temporizador, que teve como principais estudos os seguintes itens:
    Utilizar o SetInterval;
    Utilizar o ClearInterval;
    Concatenar strings e variáveis;

--- script.js
+++ script.js
@@ -5,20 +5,50 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const audioPausa = new Audio ('/sons/pause.mp3');
+const audioPlay = new Audio ('/sons/play.wav');
+const audioTempoFinalizado = new Audio ('/sons/beep.mp3');
+const startPauseBt = document.querySelector('#start-pause');
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
+
+const duracaoFoco = 1500;
+const duracaoDescansoCurto = 300;
+const duracaoDescansoLongo = 900;
+
+
+musica.loop = true;
+musicaFocoInput.addEventListener('change', () => {
+    if(musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+});
 
 focBt.addEventListener('click', () => {
-    alterarContexto('foco')
+    alterarContexto('foco');
+    focBt.classList.add('active');
 });
 
 curtoBt.addEventListener('click', () => {
-    alterarContexto('descanso-curto')
+    alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
 });
 
 longoBt.addEventListener('click', () => {
-    alterarContexto('descanso-longo')
+    alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
 });
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active');
+    });
     html.setAttribute('data-contexto', contexto)
     banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
@@ -45,6 +75,30 @@ function alterarContexto(contexto) {
     }
 }
 
-const duracaoFoco = 1500;
-const duracaoDescansoCurto = 300;
-const duracaoDescansoLongo = 900;
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        zerar();
+        audioTempoFinalizado.play();
+        alert('Tempo fiinalizado!');
+        return;
+    }
+    tempoDecorridoEmSegundos -= 1
+    console.log('temporizador: ' + tempoDecorridoEmSegundos);
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar() {
+    if (intervaloId){
+        audioPausa.play();
+        zerar();
+        return;
+    }
+    audioPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function zerar() {
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
